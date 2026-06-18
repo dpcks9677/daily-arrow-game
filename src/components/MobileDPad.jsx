d7bf63ce59886a7ddebfa2ed558bdf2b189766b8
Yechann Kim <dpcks9677@naver.com>
2026-06-19 00:29:34 +0900

    모바일 D-PAD 터치 중복 입력 버그 수정
    - onTouchStart와 onMouseDown 이벤트 핸들러가 모바일 기기에서 연달아 발생하여 오입력을 유발하던 현상 해결
    
    - 최신 표준인 onPointerDown으로 이벤트를 통합하여 한 번의 터치/클릭만 정확하게 인식하도록 변경

--- src/components/MobileDPad.jsx
+++ src/components/MobileDPad.jsx
@@ -31,30 +31,26 @@ export default function MobileDPad({ onDirectionPress }) {
     <div className="mobile-dpad-container" ref={dpadRef}>
       <button 
         className="dpad-btn dpad-up" 
-        onTouchStart={(e) => handlePress('ArrowUp', e)}
-        onMouseDown={(e) => handlePress('ArrowUp', e)}
+        onPointerDown={(e) => handlePress('ArrowUp', e)}
       >
         <ArrowUp size={32} />
       </button>
       <button 
         className="dpad-btn dpad-left" 
-        onTouchStart={(e) => handlePress('ArrowLeft', e)}
-        onMouseDown={(e) => handlePress('ArrowLeft', e)}
+        onPointerDown={(e) => handlePress('ArrowLeft', e)}
       >
         <ArrowLeft size={32} />
       </button>
       <div className="dpad-center"></div>
       <button 
         className="dpad-btn dpad-down" 
-        onTouchStart={(e) => handlePress('ArrowDown', e)}
-        onMouseDown={(e) => handlePress('ArrowDown', e)}
+        onPointerDown={(e) => handlePress('ArrowDown', e)}
       >
         <ArrowDown size={32} />
       </button>
       <button 
         className="dpad-btn dpad-right" 
-        onTouchStart={(e) => handlePress('ArrowRight', e)}
-        onMouseDown={(e) => handlePress('ArrowRight', e)}
+        onPointerDown={(e) => handlePress('ArrowRight', e)}
       >
         <ArrowRight size={32} />
       </button>
